6489300a1e2302b1947f488f32896f8067dfd4a5
ABMesbh <mesbahabde@gmail.com>
2024-10-09 05:35:34 +0200

    task2 implementation

---
+++ task2/src/implementation/CircularBuffer.java
@@ -0,0 +1,71 @@
+package implementation;
+
+/*
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright: 2017
+ *      Author: Pr. Olivier Gruber <olivier dot gruber at acm dot org>
+ */
+
+public class CircularBuffer {
+	  volatile int m_tail, m_head;
+	  volatile byte m_bytes[];
+
+	  public CircularBuffer(int capacity) {
+	    m_bytes = new byte[capacity];
+	    m_tail = m_head = 0;
+	  }
+
+	  /**
+	   * @return true if this buffer is full, false otherwise
+	   */
+	  public synchronized  boolean full() {
+	    int next = (m_head + 1) % m_bytes.length;
+	    return (next == m_tail);
+	  }
+
+	  /**
+	   * @return true if this buffer is empty, false otherwise
+	   */
+	  public synchronized  boolean empty() {
+	    return (m_tail == m_head);
+	  }
+
+	  /**
+	   * @param b: the byte to push in the buffer
+	   * @return the next available byte
+	   * @throws an IllegalStateException if full.
+	   */
+	  public synchronized  void push(byte b) {
+	    int next = (m_head + 1) % m_bytes.length;
+	    if (next == m_tail)
+	      throw new IllegalStateException();
+	    m_bytes[m_head] = b;
+	    m_head = next;
+	  }
+
+	  /**
+	   * @return the next available byte
+	   * @throws an IllegalStateException if empty.
+	   */
+	  public synchronized  byte pull() {
+	    if (m_tail == m_head)
+	      throw new IllegalStateException();
+	    int next = (m_tail + 1) % m_bytes.length;
+	    byte bits = m_bytes[m_tail];
+	    m_tail = next;
+	    return bits;
+	  }
+
+	}
---
+++ task2/src/implementation/RDV.java
@@ -0,0 +1,47 @@
+package implementation;
+
+public class RDV {
+    private ChannelImpl ac;  // accepting channel
+    private ChannelImpl cc;  // connecting channel
+    private BrokerImpl ab;   // accepting broker
+    private BrokerImpl cb;   // connecting broker
+
+    // Synchronized wait method to avoid busy waiting
+    private synchronized void waitForConnection() {
+        while (ac == null || cc == null) {
+            try {
+                wait();
+            } catch (InterruptedException ignored) {}
+        }
+    }
+
+    public synchronized ChannelImpl connect(BrokerImpl cb, int port) {
+        this.cb = cb;
+        cc = new ChannelImpl(cb, port);
+
+        if (ac != null) {
+            ac.connect(cc, cb.getName());
+            // System.out.println("Connection established between " + ab.getName() + " and " + cb.getName());
+            notifyAll();
+        } else {
+            System.out.println(cb.getName() + " is waiting for connection " );
+            waitForConnection();
+        }
+        return cc;
+    }
+
+    public synchronized ChannelImpl accept(BrokerImpl ab, int port) {
+        this.ab = ab;
+        ac = new ChannelImpl(ab, port);
+
+        if (cc != null) {
+            ac.connect(cc, ab.getName());
+            System.out.println("Connection established between " + ab.getName() + " and " + cb.getName());
+            notifyAll();
+        } else {
+            System.out.println(ab.getName() + " is waiting for connection " );
+            waitForConnection();
+        }
+        return ac;
+    }
+}
